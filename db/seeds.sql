a4c11185c9bdf249b744cd0d2b2d01e618cf3027
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2019-05-20 22:56:29 -0700

    db created

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES (BBQ Bacon Burger);
+INSERT INTO burgers (burger_name) VALUES (Angus Burger);
+INSERT INTO burgers (burger_name) VALUES (Peanute Butter & Jelly Burger);
+INSERT INTO burgers (burger_name) VALUES (Turkey Burger);
+INSERT INTO burgers (burger_name) VALUES (Pineapple Teriyaki Burger);
+INSERT INTO burgers (burger_name) VALUES (Big Boy Burger);
+INSERT INTO burgers (burger_name) VALUES (Sprout Burger);
+INSERT INTO burgers (burger_name) VALUES (Fake Meat Burger);
+INSERT INTO burgers (burger_name) VALUES (Baby Pig Burger);
