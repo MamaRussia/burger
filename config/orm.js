513266d24fc5a21aa8ceaf91fdbdf34850f1b7e6
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2019-05-20 23:07:30 -0700

    config & models completed

---
+++ config/orm.js
@@ -0,0 +1,66 @@
+const connection = require("../config/connection");
+
+var orm = {
+    all: function(burgers, cb) {
+      var queryString = "SELECT * FROM " + burgers + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    create: function(burgers, cols, vals, cb) {
+      var queryString = "INSERT INTO " + burgers;
+  
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+  
+      console.log(queryString);
+  
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+    // An example of objColVals would be {name: panther, sleepy: true}
+    update: function(burgers, objColVals, condition, cb) {
+      var queryString = "UPDATE " + burgers;
+  
+      queryString += " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+    delete: function(burgers, condition, cb) {
+      var queryString = "DELETE FROM " + burgers;
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    }
+  };
+
+  module.exports = orm;
